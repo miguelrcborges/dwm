feda88bb3556d1bcec5b36423c7ce1539919d5d3
Miguel Borges <up202004481@edu.fe.up.pt>
2024-02-18 17:44:41 +0000

    change default compiler flags

--- config.mk
+++ config.mk
@@ -24,12 +24,12 @@ FREETYPEINC = /usr/include/freetype2
 # includes and libs
 INCS = -I${X11INC} -I${FREETYPEINC}
 LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
+OPTIMIZATIONS = -D_FORTIFY_SOURCE=1 -s -march=native -O2 -ftree-vectorize -fno-semantic-interposition -pipe
 
 # flags
-CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS} -D_FORTIFY_SOURCE=1 -s -march=native -O2 \
-					 -ftree-vectorize -fno-semantic-interposition -pipe
-#CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations ${INCS} ${CPPFLAGS}
+CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS} 
+#CFLAGS   = -g -std=c99 -pedantic -Wall -Wno-deprecated-declarations -O0 ${INCS} ${CPPFLAGS}
+CFLAGS   = -std=c99 -pedantic ${INCS} ${CPPFLAGS} ${OPTIMIZATIONS}
 LDFLAGS  = ${LIBS}
 
 # Solaris
